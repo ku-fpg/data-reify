7ce2e1313c50829a33f90d971fe01574a2a61408
Brandon Moore <brandon.3.moore@gmail.com>
2013-10-14 13:39:52 -0500

    Don't use "free" package in example

--- examples/simplify.hs
+++ examples/simplify.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE DeriveFunctor, DeriveFoldable, TypeFamilies #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, TypeFamilies,
+      StandaloneDeriving, FlexibleContexts, UndecidableInstances #-}
 {-
  This example simplifies a reified graph so only nodes
  referenced from multiple places are assigned labels,
@@ -17,12 +18,19 @@ import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 
-import Control.Monad.Free(Free(Free,Pure))
 import Data.Reify(Graph(Graph),Unique)
 
 -- for the example
 import Control.Applicative(pure,liftA2)
 import Data.Reify(MuRef(mapDeRef),DeRef,reifyGraph)
+
+-- Self-contained Free monad
+data Free f a = Pure a | Free (f (Free f a))
+deriving instance (Show a, Show (f (Free f a))) => Show (Free f a)
+instance (Functor f) => Monad (Free f) where
+  return = Pure
+  Pure a >>= f = f a
+  Free m >>= f = Free (fmap (>>= f) m)
 
 newtype Hist a = Hist (Map a Int)
   deriving Show
@@ -68,6 +76,8 @@ instance MuRef (Tree a) where
   mapDeRef child (Fork l r) = liftA2 ForkF (child l) (child r)
 
 -- An example graph.
+loop1, loop2 :: Tree Int
+
 -- loop1 is referenced twice so it must have an explicit
 -- label in the simplified graph whether or not it's the root.
 loop1 = Fork (Fork (Leaf 1) loop1) loop2
